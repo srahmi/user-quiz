b8a5a85c97d4062c643dd50f3cfc0fa2adff88fc
said.rahmi <said.rahmi@argus-presse.fr>
2020-10-30 13:06:23 +0100

    show users and details

---
+++ src/app/mock-users.ts
@@ -0,0 +1,7 @@
+import {User} from './user';
+
+export const USERS: User[] = [
+  {id: 11, name: 'Saïd', email: 'srahmi@gmail.com', address: '12, Mountain view'},
+  {id: 12, name: 'Narco', email: 'narco@gmail.com', address: '1, Rue des fleurs'},
+  {id: 13, name: 'Bombasto', email: 'Bombasto@gmail.com', address: ''}
+];
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../user';
+import {USERS} from '../mock-users';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users = USERS;
+  
+  selectedUser: User;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
+
+}
